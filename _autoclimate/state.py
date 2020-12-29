006ea8b1f6dc664b493d3bd369413dd645c108f4
Ross Rosen <rrosen326@gmail.com>
2020-12-29 15:22:11 -0800

    state - I think this is working

--- _autoclimate/state.py
+++ _autoclimate/state.py
@@ -36,8 +36,6 @@ class State:
         self.test_mode = test_mode
         self.use_temp_sensors = create_temp_sensors
         self.climates = climates
-        self.hass.log(f'###state_init: {self.hass.get_app_pin()} -- thread_id: {self.hass.get_pin_thread()}')
-
 
         self.state: dict = {}
         self._current_temps: dict = {}  # {climate: current_temp}
@@ -229,7 +227,7 @@ class State:
         The logic is pretty complex due to challenges with offline/online,
         priorities, differences in behavior from different thermostats, etc.
         """
-
+        offconfig = config["off_state"]
         attributes = stateobj["attributes"] if stateobj else {}
 
         # Mocks
@@ -259,22 +257,22 @@ class State:
         # Turned off?
         if temp is None:
             # Thermostat is turned off
-            if config["off_state"] == "off":
+            if offconfig["state"] == "off":
                 return "off", "Thermostat is off", current_temp
             else:
                 return "error_off", "Thermostat is off but should not be!", current_temp
 
         # Thermostat is on.
-        elif config["off_state"] == "off":
+        elif offconfig["state"] == "off":
             return "on", "Thermostat is not off, but it should be", current_temp
 
         # Is away mode?
-        elif config["off_state"] == "away":
+        elif offconfig["state"] == "away":
             if attributes.get("preset_mode").lower() != "away":
                 return "on", "Not away mode, but should be", current_temp
             else:
                 # Proper away mode setting?
-                if (off_temp := config.get("off_temp")) is None:
+                if (off_temp := offconfig.get("temp")) is None:
                     return "off", "Away mode. No off_temp available.", current_temp
                 else:
                     if temp == off_temp:
@@ -291,17 +289,17 @@ class State:
                         )
 
         # Perm_hold?
-        elif config["off_state"] == "perm_hold":
-            if attributes.get("preset_mode") != config["perm_hold_string"]:
+        elif offconfig["state"] == "perm_hold":
+            if attributes.get("preset_mode") != offconfig["perm_hold_string"]:
                 return (
                     "on",
                     f"Not proper permanent hold. Actual: {attributes.get('preset_mode')} -- {attributes.get('temperature')}",
                     current_temp,
                 )
-            elif temp > config["off_temp"]:
+            elif temp > offconfig["temp"]:
                 return (
                     "on",
-                    f"Perm hold at {temp}. Should be <= {config['off_temp']}",
+                    f"Perm hold at {temp}. Should be <= {offconfig['temp']}",
                     current_temp,
                 )
             else:
